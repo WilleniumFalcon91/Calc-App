6e48c67116bfbbac7e76aec37594025dce5da9a8
William Elliott <wcelliott1991@gmail.com>
2017-09-05 11:26:46 -0400

    initializes calc-app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Number Cruncher</h2>
+        </div>
+        <p className="App-intro">
+          Get Ready To Crunch Some Numbers !*&^$*&
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
